c0919a4b61df8970a539cd613d72a43edffa6f58
Vitor Giovanni Cominato de Amorim <vitor.cominato@gmail.com.br>
2020-02-19 15:45:52 -0300

    Adicionando projeto

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Expanded(
+            child: FittedBox(
+              fit: BoxFit.contain, // otherwise the logo will be tiny
+              child: const FlutterLogo(),
+            ),
+          ),
+        ],
+      ),
+    );
